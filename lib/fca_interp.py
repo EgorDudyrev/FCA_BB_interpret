905ea08768c2b33add49637153ff08eda5b3dd22
Egor Dudyrev <egor.dudyrev@yandex.ru>
2020-01-15 08:00:30 +0300

    Write lib to compute concepts

--- lib/fca_interp.py
+++ lib/fca_interp.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 from tqdm import tqdm
+import concepts as concepts_mit
 
 
 class Concept:
@@ -12,8 +13,8 @@ class Concept:
 		self._up_neighbs = None
 		self._idx = idx
 		self._title = title
-		self._extent_bit = (2**self._extent).sum() if len(self._extent)>0 else 0
-		self._intent_bit = (2**self._intent).sum() if len(self._intent)>0 else 0
+		#self._extent_bit = (2**self._extent).sum() if len(self._extent)>0 else 0
+		#self._intent_bit = (2**self._intent).sum() if len(self._intent)>0 else 0
 
 	def get_extent(self):
 		return self._extent
@@ -27,17 +28,35 @@ class Concept:
 			s += f" {self._idx}"
 		if self._title is not None:
 			s += f" '{self._title}'"
+		s += f"\n"
+		s += f"extent (len: {len(self._extent)}): {','.join([f'{g}' for g in self._extent]) if len(self._extent)>0 else 'emptyset'}\n"
+		s += f"intent (len: {len(self._intent)}): {','.join([f'{m}' for m in self._intent]) if len(self._intent)>0 else 'emptyset'}\n"
+		if self._low_neighbs is not None:
+			s += f"lower neighbours (len: {len(self._low_neighbs)}): " + \
+				 f"{','.join([f'{c}' for c in self._low_neighbs]) if len(self._low_neighbs)>0 else 'emptyset'}\n"
+		if self._up_neighbs is not None:
+			s += f"upper neighbours (len: {len(self._up_neighbs)}): " + \
+				 f"{','.join([f'{c}' for c in self._up_neighbs]) if len(self._up_neighbs)>0 else 'emptyset'}\n"
+		return s
+
+	def __str__(self):
+		s = "Concept"
+		if self._idx is not None:
+			s += f" {self._idx}"
+		if self._title is not None:
+			s += f" '{self._title}'"
 		s += f": ({len(self._extent)} objs, {len(self._intent)} attrs)"
 		return s
 
 	def is_subconcept_of(self, c):
 		"""if a is subconcept of b, a<=b"""
-		#return all([g in c._extent for g in self._extent]) and all([m in self._intent for m in c._intent])
-		return self._is_subconcept_of_bit(c)
+		return all([g in c._extent for g in self._extent]) and all([m in self._intent for m in c._intent])
+		#return self._is_subconcept_of_bit(c)
 
 	def _is_subconcept_of_bit(self, c):
 		return (self._extent_bit & c._extent_bit == self._extent_bit)\
 			   and (c._intent_bit & self._intent_bit == c._intent_bit)
+
 
 class Context:
 	def __init__(self, data, objs=None, attrs=None):
@@ -60,15 +79,18 @@ class Context:
 		self._objs = np.array(objs)
 		self._attrs = np.array(attrs)
 
-		ar = np.array([2 ** i for i in range(len(self._attrs))])
-		self._objs_bit = [(g*ar).sum() for g in self._data]
-		del ar
+		#ar = np.array([2 ** i for i in range(len(self._attrs))])
+		#self._objs_bit = [(g*ar).sum() for g in self._data]
+		#del ar
 
 	def get_attrs(self):
 		return self._attrs
 
 	def get_objs(self):
 		return self._objs
+
+	def get_data(self):
+		return self._data
 
 	def get_attr_values(self, m):
 		if type(m) == int:
@@ -125,10 +147,11 @@ class Context:
 		else:
 			ms_idxs = ms
 
-		ext = self._get_extent_bitwise(ms_idxs)
-		#ext = np.arange(len(self._objs))
-		#ext = list(ext[self._data[:, ms_idxs].sum(1) == len(ms_idxs)])
-		#ext = [self._objs[g] for g in ext] if verb else ext
+		#ext = self._get_extent_bitwise(ms_idxs)
+
+		ext = np.arange(len(self._objs))
+		ext = list(ext[self._data[:, ms_idxs].sum(1) == len(ms_idxs)])
+		ext = [self._objs[g] for g in ext] if verb else ext
 		return ext
 
 	def _get_extent_bitwise(self, ms):
@@ -162,10 +185,10 @@ class Context:
 		else:
 			gs_idxs = gs
 
-		int_ = self._get_intent_bitwise(gs_idxs)
-		#int_ = np.arange(len(self._attrs))
-		#cntx = self._data[gs_idxs]
-		#int_ = list(int_[cntx.sum(0) == len(gs_idxs)])
+		#int_ = self._get_intent_bitwise(gs_idxs)
+		int_ = np.arange(len(self._attrs))
+		cntx = self._data[gs_idxs]
+		int_ = list(int_[cntx.sum(0) == len(gs_idxs)])
 		int_ = [self._attrs[m] for m in int_] if verb else int_
 		return int_
 
@@ -186,7 +209,7 @@ class Context:
 		s = f"Num of objects: {len(self._objs)}, Num of attrs: {len(self._attrs)}\n"
 		s += f"Objects: {','.join(self._objs[:5])+',...' if len(self._objs)>5 else ','.join(self._objs)}\n"
 		s += f"Attrs: {','.join(self._attrs[:5]) + ',...' if len(self._attrs) > 5 else ','.join(self._attrs)}\n"
-		s += str(self.get_table().head())
+		s += self.get_table().head().__repr__()
 		return s
 
 
@@ -204,9 +227,11 @@ class FormalManager:
 	def get_concepts(self):
 		return self._concepts
 
-	def construct_concepts(self, algo='CBO', max_iters_num=None, max_num_attrs=None, min_num_objs=None):
+	def construct_concepts(self, algo='mit', max_iters_num=None, max_num_attrs=None, min_num_objs=None, use_tqdm=True):
 		if algo == 'CBO':
-			concepts = self._close_by_one(max_iters_num, max_num_attrs, min_num_objs)
+			concepts = self._close_by_one(max_iters_num, max_num_attrs, min_num_objs, use_tqdm)
+		elif algo == 'mit':
+			concepts = self._concepts_by_mit()
 		else:
 			raise ValueError('The only supported algorithm is CBO (CloseByOne')
 
@@ -217,23 +242,34 @@ class FormalManager:
 		self._top_concept = [c for c in concepts if len(c.get_extent()) == len(self._context.get_objs())][0]
 		self._bottom_concept = [c for c in concepts if len(c.get_intent()) == len(self._context.get_attrs())][0]
 
-	def _close_by_one(self, max_iters_num=None, max_num_attrs=None, min_num_objs=None):
+	def _concepts_by_mit(self):
+		cntx_mit = concepts_mit.Context([f"g{x}" for x in range(len(self._context.get_objs()))],
+										[f"m{x}" for x in range(len(self._context.get_attrs()))],
+										self._context.get_data()
+										)
+
+		concepts = {Concept(ext_, int_) for ext_, int_ in cntx_mit.lattice}
+		return concepts
+
+	def _close_by_one(self, max_iters_num, max_num_attrs, min_num_objs, use_tqdm):
 		cntx = self._context
 		n_attrs = len(cntx.get_attrs())
 		combs_to_check = [[]]
 		concepts = set()
-		if max_num_attrs is not None and min_num_objs is not None:
-			tot = min(sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, max_num_attrs + 1)]),
-					  sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, min_num_objs + 1)]))
-			t = tqdm(total=tot)
-		elif max_num_attrs is not None:
-			t = tqdm(total=sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, max_num_attrs + 1)]))
-		elif min_num_objs is not None:
-			t = tqdm(total=sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, min_num_objs + 1)]))
-		else:
-			t = tqdm(total=len(combs_to_check))
+		if use_tqdm:
+			if max_num_attrs is not None and min_num_objs is not None:
+				tot = min(sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, max_num_attrs + 1)]),
+						  sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, min_num_objs + 1)]))
+				t = tqdm(total=tot)
+			elif max_num_attrs is not None:
+				t = tqdm(total=sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, max_num_attrs + 1)]))
+			elif min_num_objs is not None:
+				t = tqdm(total=sum([sp.misc.comb(N=n_attrs, k=x) for x in range(0, min_num_objs + 1)]))
+			else:
+				t = tqdm(total=len(combs_to_check))
 
 		iter = 0
+		saved_ints = set()
 		while len(combs_to_check) > 0:
 			iter += 1
 			if max_iters_num is not None and iter >= max_iters_num:
@@ -249,54 +285,46 @@ class FormalManager:
 
 			new_int_ = [x for x in int_ if x not in comb]
 
-			if len(comb) > 0 and any([x < comb[-1] for x in new_int_]):
-				t.update()
+			if (len(comb) > 0 and any([x < comb[-1] for x in new_int_])) or tuple(int_) in saved_ints:
+				if use_tqdm:
+					t.update()
 				continue
 
 			concepts.add(Concept(ext_, int_))
-			new_combs = [int_ + [x] for x in range((int_[-1] if len(int_) > 0 else -1) + 1, n_attrs+2)]
+			saved_ints.add(tuple(int_))
+			new_combs = [int_ + [x] for x in range((comb[-1] if len(comb) > 0 else -1) + 1, n_attrs) if x not in int_]
 			combs_to_check = new_combs + combs_to_check
-			t.update()
-			if max_num_attrs is None or min_num_objs is not None:
-				t.total += len(new_combs)
+			if use_tqdm:
+				t.update()
+				if max_num_attrs is None or min_num_objs is not None:
+					t.total += len(new_combs)
 
-		int_ = cntx.get_intent([], trust_mode=True, verb=False)
-		ext_ = cntx.get_extent(int_, trust_mode=True, verb=False)
-		concepts.add(Concept(ext_, int_))
+		if tuple([]) not in saved_ints:
+			int_ = cntx.get_intent([], trust_mode=True, verb=False)
+			ext_ = cntx.get_extent(int_, trust_mode=True, verb=False)
+			concepts.add(Concept(ext_, int_))
 
 		return concepts
 
-	def construct_lattice(self, use_tqdm=True):
-		concepts_set = sorted(self._concepts, key=lambda c: len(c.get_extent()))
-		concepts_map = {k: i for i, k in enumerate(concepts_set)}
-		concepts_map_inv = {v: k for k, v in concepts_map.items()}
-		lower_neighbours = {concepts_map[concepts_set[0]]: set()}
-		max_lowest_neighbours = {concepts_map[concepts_set[0]]: set()}
+	def construct_lattice_connections(self, use_tqdm=True):
+		n_concepts = len(self._concepts)
+		cncpts_map = {c._idx:c for c in self._concepts}
+		all_low_neighbs = {c._idx:set() for c in self._concepts}
 
-		for idx, concept in tqdm(enumerate(concepts_set[1:]), total=len(concepts_set) - 1, disable=not use_tqdm):
-			possible_neighbs = set([concepts_map[pn] for pn in concepts_set[:idx]])
-			neighbs = set()
-			ml_neighbs = set()
+		for cncpt_idx in tqdm(range(n_concepts-1, -1, -1), disable=not use_tqdm):
+			concept = cncpts_map[cncpt_idx]
+			concept._low_neighbs = set()
+			possible_neighbs = set(range(cncpt_idx+1, n_concepts))
 
-			while len(possible_neighbs) > 0:
-				pn_idx = np.max(possible_neighbs)
-				if type(pn_idx) == set:
-					pn_idx = list(pn_idx)[0]
+			while len(possible_neighbs)>0:
+				pn_idx = min(possible_neighbs)
 				possible_neighbs.remove(pn_idx)
-				pn = concepts_map_inv[pn_idx]
 
-				if pn.is_subconcept_of(concept):
-					possible_neighbs = possible_neighbs - lower_neighbours[concepts_map[pn]]
-					neighbs = neighbs | {pn_idx} | lower_neighbours[concepts_map[pn]]
-					ml_neighbs.add(pn_idx)
+				if cncpts_map[pn_idx].is_subconcept_of(concept):
+					all_low_neighbs[cncpt_idx] = all_low_neighbs[cncpt_idx]|{pn_idx}|all_low_neighbs[pn_idx]
+					concept._low_neighbs.add(pn_idx)
+					possible_neighbs = possible_neighbs-all_low_neighbs[pn_idx]
 
-			lower_neighbours[concepts_map[concept]] = neighbs
-			max_lowest_neighbours[concepts_map[concept]] = ml_neighbs
-
-		max_lowest_neighbours = {concepts_map_inv[k]: tuple([concepts_map_inv[v_] for v_ in v]) for k, v in
-								 max_lowest_neighbours.items()}
-
-		for c in self._concepts:
-			c._low_neighbs = max_lowest_neighbours[c]
-			for ln in c._low_neighbs:
-				ln._up_neighbs = [c] if ln._up_neighbs is None else ln._up_neighbs+[c]
+			concept._up_neighbs = set()
+			for ln_idx in concept._low_neighbs:
+				cncpts_map[ln_idx]._up_neighbs.add(concept._idx)
